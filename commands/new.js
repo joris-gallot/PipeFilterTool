61b251f79b5ed07e94eb7fb329b172d90a4f454e
Joris <you@example.com>
2021-11-19 15:59:39 +0100

    remove console log

--- commands/new.js
+++ commands/new.js
@@ -4,7 +4,6 @@ const path = require("path");
 const PROJECT_TEMPLATE_PATH = path.resolve(__dirname, "..", "project-template");
 
 module.exports = (projectName) => {
-  console.log(projectName);
   fs.copy(PROJECT_TEMPLATE_PATH, "./" + projectName, (err) => {
     if (err) {
       console.log("An error occured while creating the project.");
